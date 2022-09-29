eb8def0d60ac4d378cb95620c21d872b1062fcba
RLettuce <syunrobin@gmail.com>
2022-09-29 23:11:11 +0900

    init

---
+++ program.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+
+def sigma(*arg):
+    while type(arg[0]) is tuple:
+        arg = arg[0]
+    ans = 0
+    for i in range(len(arg)):
+        ans += arg[i]
+    return ans
+
+def ave(*arg):
+    while type(arg[0]) is tuple:
+        arg = arg[0]
+    return sigma(arg) / len(arg)
+
+def squared_avarage_standard_error(*arg):
+    average = ave(arg)
+    n = len(arg)
+    RSS = 0
+    for i in range(n):
+        RSS += (arg[i] - average) ** 2
+    return RSS / n * (n - 1)
